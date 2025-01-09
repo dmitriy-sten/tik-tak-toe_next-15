de39439dc5eb0a63ee76f06749b5bd8b0e8084fc
vsegda_vinovat <riabko97@gmail.com>
2025-01-09 20:21:26 +0200

    added link to game

--- src/features/games-list/containers/games-list.tsx
+++ src/features/games-list/containers/games-list.tsx
@@ -2,7 +2,6 @@ import { getIdleGames } from "@/entities/game/server";
 import { Layout } from "../ui/layout";
 import { GameCard } from "../ui/game-card";
 import { CreateButton } from "./create-button";
-import { createGameAction } from "../actions/create-game";
 import { Button } from "@/shared/ui/button";
 import Link from "next/link";
 import { routes } from "@/kernel/routes";
@@ -18,9 +17,9 @@ export async function GamesList() {
           rating={game.creator.rating}
           key={game.id}
           actions={
-            <Button asChild>
-              <Link href={routes.game(game.id)}>Connect</Link>
-            </Button>
+            <Link href={routes.game(game.id)}>
+              <Button>Connect</Button>
+            </Link>
           }
         />
       ))}
